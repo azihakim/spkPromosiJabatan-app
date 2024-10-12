703d8d1064c29c6fb114b2159f89aaeeadc4c758
Muhammad Abdul Azizul Hakim <113509874+azihakim@users.noreply.github.com>
2024-10-12 15:28:06 +0700

    d

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>PT. CAL - SPK</title>
+	<title>PT. BPR SYARIAH AL-FALAH</title>
 
 	<link href="{{ asset('vendors/resource/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
 	<!-- Font Awesome -->

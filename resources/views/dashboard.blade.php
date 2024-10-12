8fa0355d60e2d3c2e328511e0c5f4d4028efa23b
Muhammad Abdul Azizul Hakim <113509874+azihakim@users.noreply.github.com>
2024-10-12 15:22:19 +0700

    push

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,7 +51,7 @@
 			<div class="x_content">
 				<div class="row">
 					<div class="col-sm-12">
-						<div class="card-box table-responsive">
+						<div class="card-box table">
 							<div class="text-center">
 								{{-- <img src="{{ asset('vendors/build/images/logo.png') }}" style="width: 150px"> --}}
 								<h1>Selamat Datang</h1>
@@ -60,56 +60,188 @@
 								<hr>
 								{{-- <h2>Kriteria Penilaian dan Indikator Penilaian Karyawan</h2> --}}
 							</div>
-							{{-- <div class="row">
-								<div class="col-6">
-									<table class="table">
+							<div class="row ">
+								<img style="width: 60%" src="{{ asset('vendors/build/images/struktur.png') }}" alt="" class="mx-auto">
+							</div>
+							<br>
+							<div class="text-center">
+								<hr>
+								<h2>Penilaian SPK</h2>
+							</div>
+							<div class="row">
+								<div class="col-3">
+									<table border="1" cellpadding="5" cellspacing="0">
 										<thead>
-											<tr>
-												<th>Kriteria</th>
-												<th>Sub Kriteria</th>
+											<tr style="background-color: #c3e6cb;">
+												<th>Kode</th>
+												<th>Keterangan</th>
 											</tr>
 										</thead>
 										<tbody>
-											@foreach ($data as $item)
-												<tr>
-													<td>{{ $item->nama }}</td>
-													<td>
-														<ul>
-															@foreach ($item->subKriterias as $sub)
-																<li>{{ $sub->nama }}</li>
-															@endforeach
-														</ul>
-													</td>
-												</tr>
-											@endforeach
+											<tr>
+												<td>C1</td>
+												<td>Tingkat Pendidikan</td>
+											</tr>
+											<tr>
+												<td>C2</td>
+												<td>Kompetensi</td>
+											</tr>
+											<tr>
+												<td>C3</td>
+												<td>Tekanan Waktu</td>
+											</tr>
+											<tr>
+												<td>C4</td>
+												<td>Absensi</td>
+											</tr>
+											<tr>
+												<td>C5</td>
+												<td>Tanggung Jawab Pekerjaan</td>
+											</tr>
 										</tbody>
 									</table>
+
 								</div>
-								<div class="col-6">
-									<table class="formula-table">
-										<tr>
-											<th colspan="2">Rumus</th>
-										</tr>
-										<tr>
-											<td colspan="2">Nilai sub Kriteria 1 = Nilai Kehadiran × Bobot Sub 1</td>
-										</tr>
-										<tr>
-											<td colspan="2">Nilai sub Kriteria 2 = Nilai Kehadiran × Bobot Sub 2</td>
-										</tr>
-										<tr>
-											<td colspan="2">Nilai sub Kriteria 3 = Nilai Kehadiran × Bobot Sub 3</td>
-										</tr>
-										<tr>
-											<td colspan="2">Nilai Akhir Kriteria = Nilai sub Kriteria 1 + Nilai sub
-												Kriteria 2 + Skor Sub Kriteria 3</td>
-										</tr>
-										<tr>
-											<td colspan="2">Nilai Karyawan = <br> (Nilai Akhir Kriteria × Bobot Utama) /
-												Jumlah Sub Kriteria</td>
-										</tr>
+								<div class="col-3">
+									<table border="1" cellpadding="5" cellspacing="0">
+										<thead>
+											<tr style="background-color: #c3e6cb;">
+												<th>Kode</th>
+												<th>Keterangan</th>
+												<th>Nilai</th>
+											</tr>
+										</thead>
+										<tbody>
+											<!-- Tingkat Pendidikan -->
+											<tr>
+												<td colspan="3" style="background-color: #f8f9fa; text-align: center;">Tingkat Pendidikan</td>
+											</tr>
+											<tr style="background-color: #f8d7da;">
+												<td>C1.1</td>
+												<td>SMA/SMK</td>
+												<td>1</td>
+											</tr>
+											<tr style="background-color: #f8d7da;">
+												<td>C1.2</td>
+												<td>D3/S1</td>
+												<td>2</td>
+											</tr>
+											<tr style="background-color: #f8d7da;">
+												<td>C1.3</td>
+												<td>S1/S2</td>
+												<td>3</td>
+											</tr>
+
+											<!-- Kompetensi -->
+											<tr>
+												<td colspan="3" style="background-color: #f8f9fa; text-align: center;">Kompetensi</td>
+											</tr>
+											<tr style="background-color: #fff3cd;">
+												<td>C2.1</td>
+												<td>&lt;70-80</td>
+												<td>1</td>
+											</tr>
+											<tr style="background-color: #fff3cd;">
+												<td>C2.2</td>
+												<td>81-90</td>
+												<td>2</td>
+											</tr>
+											<tr style="background-color: #fff3cd;">
+												<td>C2.3</td>
+												<td>91-100</td>
+												<td>3</td>
+											</tr>
+										</tbody>
 									</table>
+
 								</div>
-							</div> --}}
+								<div class="col-3">
+									<table border="1" cellpadding="5" cellspacing="0">
+										<thead>
+											<tr style="background-color: #c3e6cb;">
+												<th>Kode</th>
+												<th>Keterangan</th>
+												<th>Nilai</th>
+											</tr>
+										</thead>
+										<tbody>
+											<!-- Tekanan Waktu -->
+											<tr>
+												<td colspan="3" style="background-color: #f8f9fa; text-align: center;">Tekanan Waktu</td>
+											</tr>
+											<tr style="background-color: #d4edda;">
+												<td>C3.1</td>
+												<td>1-5</td>
+												<td>1</td>
+											</tr>
+											<tr style="background-color: #d4edda;">
+												<td>C3.2</td>
+												<td>6-12</td>
+												<td>2</td>
+											</tr>
+											<tr style="background-color: #d4edda;">
+												<td>C3.3</td>
+												<td>&gt;12</td>
+												<td>3</td>
+											</tr>
+
+											<!-- Absensi -->
+											<tr>
+												<td colspan="3" style="background-color: #f8f9fa; text-align: center;">Absensi</td>
+											</tr>
+											<tr style="background-color: #cce5ff;">
+												<td>C4.1</td>
+												<td>75-80%</td>
+												<td>1</td>
+											</tr>
+											<tr style="background-color: #cce5ff;">
+												<td>C4.2</td>
+												<td>81-85%</td>
+												<td>2</td>
+											</tr>
+											<tr style="background-color: #cce5ff;">
+												<td>C4.3</td>
+												<td>&gt;85%</td>
+												<td>3</td>
+											</tr>
+										</tbody>
+									</table>
+
+								</div>
+								<div class="col-3">
+									<table border="1" cellpadding="5" cellspacing="0">
+										<thead>
+											<tr style="background-color: #c3e6cb;">
+												<th>Kode</th>
+												<th>Keterangan</th>
+												<th>Nilai</th>
+											</tr>
+										</thead>
+										<tbody>
+											<!-- Tanggung Jawab Pekerjaan -->
+											<tr>
+												<td colspan="3" style="background-color: #f8f9fa; text-align: center;">Tanggung Jawab Pekerjaan</td>
+											</tr>
+											<tr style="background-color: #ffeeba;">
+												<td>C5.1</td>
+												<td>Rendah</td>
+												<td>1</td>
+											</tr>
+											<tr style="background-color: #ffeeba;">
+												<td>C5.2</td>
+												<td>Sedang</td>
+												<td>2</td>
+											</tr>
+											<tr style="background-color: #ffeeba;">
+												<td>C5.3</td>
+												<td>Tinggi</td>
+												<td>3</td>
+											</tr>
+										</tbody>
+									</table>
+
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>

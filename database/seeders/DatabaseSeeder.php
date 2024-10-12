6cbf5d2694a4ef0f56cedcb3caef179f5c18f76f
Muhammad Abdul Azizul Hakim <113509874+azihakim@users.noreply.github.com>
2024-10-12 14:54:59 +0700

    push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,309 +15,309 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
- /**
-  * Seed the application's database.
-  */
- public function run(): void
- {
-  // \App\Models\User::factory(10)->create();
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
 
-  // \App\Models\User::factory()->create([
-  //     'name' => 'Test User',
-  //     'email' => 'test@example.com',
-  // ]);
-  User::create([
-   'name' => 'HRD',
-   'role' => 'hrd',
-   'username' => 'hrd',
-   'password' => Hash::make('123'),
-  ]);
-  User::create([
-   'name' => 'Pimpinan',
-   'role' => 'pimpinan',
-   'username' => 'pimpinan',
-   'password' => Hash::make('123'),
-  ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        User::create([
+            'name' => 'HRD',
+            'role' => 'hrd',
+            'username' => 'hrd',
+            'password' => Hash::make('123'),
+        ]);
+        User::create([
+            'name' => 'Pimpinan',
+            'role' => 'pimpinan',
+            'username' => 'pimpinan',
+            'password' => Hash::make('123'),
+        ]);
 
-  $karyawan = [
-   [
-    'nama' => 'Hanny',
-    'jabatan' => 'IT'
-   ],
-   [
-    'nama' => 'Farhan',
-    'jabatan' => 'IT'
-   ],
-   [
-    'nama' => 'Desi',
-    'jabatan' => 'IT'
-   ],
-   [
-    'nama' => 'Amelia',
-    'jabatan' => 'IT'
-   ],
-   [
-    'nama' => 'Kevin',
-    'jabatan' => 'IT'
-   ],
-   [
-    'nama' => 'Alda',
-    'jabatan' => 'Marketing'
-   ],
-   [
-    'nama' => 'Devi',
-    'jabatan' => 'Marketing'
-   ],
-   [
-    'nama' => 'Dian',
-    'jabatan' => 'Marketing'
-   ],
-   [
-    'nama' => 'Anto',
-    'jabatan' => 'Marketing'
-   ],
-   [
-    'nama' => 'Abi',
-    'jabatan' => 'Marketing'
-   ],
-   [
-    'nama' => 'Imam',
-    'jabatan' => 'Operasional'
-   ],
-   [
-    'nama' => 'Ridwan',
-    'jabatan' => 'Operasional'
-   ],
-   [
-    'nama' => 'Dewi',
-    'jabatan' => 'Operasional'
-   ],
-   [
-    'nama' => 'Amar',
-    'jabatan' => 'Operasional'
-   ],
-   [
-    'nama' => 'Sari',
-    'jabatan' => 'Operasional'
-   ],
-   [
-    'nama' => 'Zaki',
-    'jabatan' => 'Manajemen Resiko'
-   ],
-   [
-    'nama' => 'Rizki',
-    'jabatan' => 'Manajemen Resiko'
-   ],
-   [
-    'nama' => 'Ridho',
-    'jabatan' => 'Manajemen Resiko'
-   ],
-   [
-    'nama' => 'Zize',
-    'jabatan' => 'Manajemen Resiko'
-   ],
-   [
-    'nama' => 'Shella',
-    'jabatan' => 'Manajemen Resiko'
-   ],
-   [
-    'nama' => 'Wisnu',
-    'jabatan' => 'Audit'
-   ],
-   [
-    'nama' => 'Adit',
-    'jabatan' => 'Audit'
-   ],
-   [
-    'nama' => 'Loli',
-    'jabatan' => 'Audit'
-   ],
-   [
-    'nama' => 'Vadel',
-    'jabatan' => 'Audit'
-   ],
-   [
-    'nama' => 'Mayang',
-    'jabatan' => 'Audit'
-   ],
-  ];
+        $karyawan = [
+            [
+                'nama' => 'Hanny',
+                'jabatan' => 'IT'
+            ],
+            [
+                'nama' => 'Farhan',
+                'jabatan' => 'IT'
+            ],
+            [
+                'nama' => 'Desi',
+                'jabatan' => 'IT'
+            ],
+            [
+                'nama' => 'Amelia',
+                'jabatan' => 'IT'
+            ],
+            [
+                'nama' => 'Kevin',
+                'jabatan' => 'IT'
+            ],
+            [
+                'nama' => 'Alda',
+                'jabatan' => 'Marketing'
+            ],
+            [
+                'nama' => 'Devi',
+                'jabatan' => 'Marketing'
+            ],
+            [
+                'nama' => 'Dian',
+                'jabatan' => 'Marketing'
+            ],
+            [
+                'nama' => 'Anto',
+                'jabatan' => 'Marketing'
+            ],
+            [
+                'nama' => 'Abi',
+                'jabatan' => 'Marketing'
+            ],
+            [
+                'nama' => 'Imam',
+                'jabatan' => 'Operasional'
+            ],
+            [
+                'nama' => 'Ridwan',
+                'jabatan' => 'Operasional'
+            ],
+            [
+                'nama' => 'Dewi',
+                'jabatan' => 'Operasional'
+            ],
+            [
+                'nama' => 'Amar',
+                'jabatan' => 'Operasional'
+            ],
+            [
+                'nama' => 'Sari',
+                'jabatan' => 'Operasional'
+            ],
+            [
+                'nama' => 'Zaki',
+                'jabatan' => 'Manajemen Resiko'
+            ],
+            [
+                'nama' => 'Rizki',
+                'jabatan' => 'Manajemen Resiko'
+            ],
+            [
+                'nama' => 'Ridho',
+                'jabatan' => 'Manajemen Resiko'
+            ],
+            [
+                'nama' => 'Zize',
+                'jabatan' => 'Manajemen Resiko'
+            ],
+            [
+                'nama' => 'Shella',
+                'jabatan' => 'Manajemen Resiko'
+            ],
+            [
+                'nama' => 'Wisnu',
+                'jabatan' => 'Audit'
+            ],
+            [
+                'nama' => 'Adit',
+                'jabatan' => 'Audit'
+            ],
+            [
+                'nama' => 'Loli',
+                'jabatan' => 'Audit'
+            ],
+            [
+                'nama' => 'Vadel',
+                'jabatan' => 'Audit'
+            ],
+            [
+                'nama' => 'Mayang',
+                'jabatan' => 'Audit'
+            ],
+        ];
 
-  foreach ($karyawan as $k) {
-   Karyawan::create([
-    'nama' => $k['nama'],
-    'jabatan' => $k['jabatan'],
-   ]);
-  }
+        foreach ($karyawan as $k) {
+            Karyawan::create([
+                'nama' => $k['nama'],
+                'jabatan' => $k['jabatan'],
+            ]);
+        }
 
-  $kriteria = [
-   [
-    'nama' => 'Tingkat Pendidikan',
-    'kode' => 'C1',
-   ],
-   [
-    'nama' => 'Kompetensi',
-    'kode' => 'C2',
-   ],
-   [
-    'nama' => 'Tekanan Waktu',
-    'kode' => 'C3',
-   ],
-   [
-    'nama' => 'Absensi',
-    'kode' => 'C4',
-   ],
-   [
-    'nama' => 'Tanggung Jawab',
-    'kode' => 'C5',
-   ],
-  ];
+        $kriteria = [
+            [
+                'nama' => 'Tingkat Pendidikan',
+                'kode' => 'C1',
+            ],
+            [
+                'nama' => 'Kompetensi',
+                'kode' => 'C2',
+            ],
+            [
+                'nama' => 'Tekanan Waktu',
+                'kode' => 'C3',
+            ],
+            [
+                'nama' => 'Absensi',
+                'kode' => 'C4',
+            ],
+            [
+                'nama' => 'Tanggung Jawab',
+                'kode' => 'C5',
+            ],
+        ];
 
-  foreach ($kriteria as $k) {
-   Kriteria::create([
-    'nama' => $k['nama'],
-    'kode' => $k['kode'],
-   ]);
-  }
+        foreach ($kriteria as $k) {
+            Kriteria::create([
+                'nama' => $k['nama'],
+                'kode' => $k['kode'],
+            ]);
+        }
 
 
-  $subKriteria = [
-   // Sub-kriteria untuk Kriteria C1
-   [
-    'kriteria_id' => 1, // Kriteria 'Tingkat Pendidikan'
-    'kode' => 'C1.1',
-    'rentang' => 'SMA/SMK',
-    'bobot' => '1',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-   [
-    'kriteria_id' => 1,
-    'kode' => 'C1.2',
-    'rentang' => 'D3',
-    'bobot' => '2',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-   [
-    'kriteria_id' => 1,
-    'kode' => 'C1.3',
-    'rentang' => 'S1',
-    'bobot' => '3',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-   [
-    'kriteria_id' => 2, // Kriteria 'Kompetensi'
-    'kode' => 'C2.1',
-    'rentang' => '≤70-80',
-    'bobot' => '1',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-   [
-    'kriteria_id' => 2,
-    'kode' => 'C2.2',
-    'rentang' => '81-90',
-    'bobot' => '2',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-   [
-    'kriteria_id' => 2,
-    'kode' => 'C2.3',
-    'rentang' => '91-100',
-    'bobot' => '3',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
+        $subKriteria = [
+            // Sub-kriteria untuk Kriteria C1
+            [
+                'kriteria_id' => 1, // Kriteria 'Tingkat Pendidikan'
+                'kode' => 'C1.1',
+                'rentang' => 'SMA/SMK',
+                'bobot' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_id' => 1,
+                'kode' => 'C1.2',
+                'rentang' => 'D3/S1',
+                'bobot' => '2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_id' => 1,
+                'kode' => 'C1.3',
+                'rentang' => 'S1/S2',
+                'bobot' => '3',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_id' => 2, // Kriteria 'Kompetensi'
+                'kode' => 'C2.1',
+                'rentang' => '≤70-80',
+                'bobot' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_id' => 2,
+                'kode' => 'C2.2',
+                'rentang' => '81-90',
+                'bobot' => '2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_id' => 2,
+                'kode' => 'C2.3',
+                'rentang' => '91-100',
+                'bobot' => '3',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
 
-   // Sub-kriteria untuk Kriteria C3
-   [
-    'kriteria_id' => 3, // Kriteria 'Tekanan Waktu'
-    'kode' => 'C3.1',
-    'rentang' => '1-5',
-    'bobot' => '1',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-   [
-    'kriteria_id' => 3,
-    'kode' => 'C3.2',
-    'rentang' => '6-12',
-    'bobot' => '2',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-   [
-    'kriteria_id' => 3,
-    'kode' => 'C3.3',
-    'rentang' => '≥12',
-    'bobot' => '3',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
+            // Sub-kriteria untuk Kriteria C3
+            [
+                'kriteria_id' => 3, // Kriteria 'Tekanan Waktu'
+                'kode' => 'C3.1',
+                'rentang' => '1-5',
+                'bobot' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_id' => 3,
+                'kode' => 'C3.2',
+                'rentang' => '6-12',
+                'bobot' => '2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_id' => 3,
+                'kode' => 'C3.3',
+                'rentang' => '≥12',
+                'bobot' => '3',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
 
-   // Sub-kriteria untuk Kriteria C4
-   [
-    'kriteria_id' => 4, // Kriteria 'Absensi'
-    'kode' => 'C4.1',
-    'rentang' => '75-80%',
-    'bobot' => '1',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-   [
-    'kriteria_id' => 4,
-    'kode' => 'C4.2',
-    'rentang' => '81-85%',
-    'bobot' => '2',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-   [
-    'kriteria_id' => 4,
-    'kode' => 'C4.2',
-    'rentang' => '≥85%',
-    'bobot' => '2',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
+            // Sub-kriteria untuk Kriteria C4
+            [
+                'kriteria_id' => 4, // Kriteria 'Absensi'
+                'kode' => 'C4.1',
+                'rentang' => '75-80%',
+                'bobot' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_id' => 4,
+                'kode' => 'C4.2',
+                'rentang' => '81-85%',
+                'bobot' => '2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_id' => 4,
+                'kode' => 'C4.2',
+                'rentang' => '≥85%',
+                'bobot' => '2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
 
-   // Sub-kriteria untuk Kriteria C5
-   [
-    'kriteria_id' => 5, // Kriteria 'Tanggung Jawab'
-    'kode' => 'C5.1',
-    'rentang' => 'Rendah',
-    'bobot' => '1',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-   [
-    'kriteria_id' => 5,
-    'kode' => 'C5.2',
-    'rentang' => 'Sedang',
-    'bobot' => '2',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-   [
-    'kriteria_id' => 5,
-    'kode' => 'C5.3',
-    'rentang' => 'Rendah',
-    'bobot' => '3',
-    'created_at' => now(),
-    'updated_at' => now(),
-   ],
-  ];
+            // Sub-kriteria untuk Kriteria C5
+            [
+                'kriteria_id' => 5, // Kriteria 'Tanggung Jawab'
+                'kode' => 'C5.1',
+                'rentang' => 'Rendah',
+                'bobot' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_id' => 5,
+                'kode' => 'C5.2',
+                'rentang' => 'Sedang',
+                'bobot' => '2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_id' => 5,
+                'kode' => 'C5.3',
+                'rentang' => 'Tinggi',
+                'bobot' => '3',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
 
-  // Menyimpan data ke tabel sub_kriteria
-  foreach ($subKriteria as $sk) {
-   SubKriteria::create([
-    'kriteria_id' => $sk['kriteria_id'],
-    'kode' => $sk['kode'],
-    'rentang' => $sk['rentang'],
-    'bobot' => $sk['bobot'],
-   ]);
-  }
- }
+        // Menyimpan data ke tabel sub_kriteria
+        foreach ($subKriteria as $sk) {
+            SubKriteria::create([
+                'kriteria_id' => $sk['kriteria_id'],
+                'kode' => $sk['kode'],
+                'rentang' => $sk['rentang'],
+                'bobot' => $sk['bobot'],
+            ]);
+        }
+    }
 }
